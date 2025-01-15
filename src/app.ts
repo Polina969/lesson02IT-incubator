487ebec396a8ffc5418dc425aad1acf76ab4efb1
PolinaShih <polina74898@gmail.com>
2025-01-16 02:42:12 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import { BlogsRouter, VersionRouter } from "./router/blogs";
 import { dbBlogs } from "./db/dbBlogs";
 import cors from "cors";
+import { deleteTestsRouter } from "./router/testing";
 
 export const app = express();
 export const PORT = 2010;
@@ -10,3 +11,4 @@ app.use(jsonBodyMiddleware);
 
 app.use(VersionRouter());
 app.use("/blogs", BlogsRouter(dbBlogs));
+app.use(deleteTestsRouter());
